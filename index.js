5874703fa1d0413e0af50103d0d565810476e31f
aelmardhi <aelmardhi@gmail.com>
2022-08-06 21:21:41 +0200

    error handler

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static('build'))
 app.get('*',(req,res)=>{
-    res.sendFile('./build/index.html')
+    res.sendFile(__dirname+'/build/index.html')
 })
 
 const PORT = process.env.PORT || 3000;
