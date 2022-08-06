a65c58bd009f6513cdf147173eebfbcb69b3dd38
aelmardhi <aelmardhi@gmail.com>
2022-08-06 17:00:47 +0200

    color amount

--- src/pages/Account/TransactionListItem.js
+++ src/pages/Account/TransactionListItem.js
@@ -4,7 +4,11 @@ import DeleteTransaction from "./DeleteTransaction";
 export default function TransactionsListItem (props){
     return(
         <tr>
-            <td>{props.transaction.amount}</td>
+            <td className={
+                props.transaction.amount > 0 ? 'positive':
+                props.transaction.amount < 0 ? 'negative':
+                                                ''
+            }>{props.transaction.amount}</td>
             <td>{new Date(props.transaction.date).toLocaleDateString()}</td>
             <td>{props.transaction.details}</td>
             <td><a className="btn pen" href={"/account/"+props.accountId+"/editTransaction/"+props.transaction.id}><PenIcon/></a></td>
--- src/pages/Home/AccountListItem.js
+++ src/pages/Home/AccountListItem.js
@@ -4,7 +4,11 @@ export default function AccountsListItem (props){
         <li>
             <a href={'/account/'+props.account.id}>
                 <span>{props.account.name} </span>
-                <span>{props.account.total}</span>
+                <span className={
+                    props.account.total > 0? 'positive':
+                    props.account.total < 0? 'negative':
+                                                ''
+                }>{props.account.total}</span>
             </a>
         </li>
     );
