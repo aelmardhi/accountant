019baa50a3b5be574113b9d1b98f8bac31a0e661
aelmardhi <aelmardhi@gmail.com>
2022-08-09 16:01:03 +0200

    add debt checkbox

--- src/pages/EditTeansaction/index.js
+++ src/pages/EditTeansaction/index.js
@@ -7,6 +7,7 @@ export default function EditTransaction(props){
     const navigate = useNavigate()
     const [transaction , setTransaction] = useState({
                                                     amount :0,
+                                                    amountNegative: false, 
                                                     details: '',
                                                 })
     const accountId = parseInt(useParams().accountId)
@@ -19,6 +20,9 @@ export default function EditTransaction(props){
             })
         getTransaction(accountId,transactionId)
             .then(t=>{
+                if (t.amount < 0)
+                    t.amountNegative = true;
+                    t.amount = Math.abs(t.amount)
                 setTransaction(t);
             })
     },[accountId,transactionId]);
@@ -31,18 +35,24 @@ export default function EditTransaction(props){
             </fieldset>
             <fieldset>
                 <label htmlFor="amount">Amount</label>
-                <input name="amount" id="amount" type="number" value={transaction.amount} onChange={(e)=>{
-                    setTransaction({amount: parseInt(e.target.value), details: transaction.details});
+                <input name="amount" id="amount" type="number" min="0" value={Math.abs(transaction.amount)} onChange={(e)=>{
+                    
+                    setTransaction({amount: Math.abs(parseInt(e.target.value)), amountNegative:transaction.amountNegative, details: transaction.details});
+                }}></input>
+                <input name="amountNegative" className="amountNegative" id="amountNegative" type="checkbox" checked={transaction.amountNegative} onChange={(e)=>{
+                    setTransaction({amount: transaction.amount, amountNegative:e.target.checked, details: transaction.details});
                 }}></input>
             </fieldset>
             <fieldset>
                 <label htmlFor="details">Details</label>
                 <textarea name="details" id="details" value={transaction.details} onChange={(e)=>{
-                    setTransaction({details: e.target.value, amount: transaction.amount});
+                    setTransaction({details: e.target.value, amount: transaction.amount, amountNegative:transaction.amountNegative});
                 }}></textarea>
             </fieldset>
             <button className="btn" onClick={(e)=>{
                 // transaction.date = Date();
+                if (transaction.amountNegative)
+                    transaction.amount *= -1;
                 updateTransaction(accountId, transactionId,transaction.amount,transaction.date, transaction.details).then(r=>{
                     navigate(-1)
                 })
